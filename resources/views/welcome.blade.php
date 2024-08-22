324f4b1a399c7031b28280087ef4c290e6bc3124
eyacdotes <eyaclemarc@gmail.com>
2024-08-23 02:52:43 +0800

    favicon to login and register

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
+        <link rel="icon" href="{{ asset('place-it.png') }}" type="image/png">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
