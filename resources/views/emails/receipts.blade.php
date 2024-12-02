136f4eb82b5e9e789d86347ad792837f9cf58929
Perod122 <jasperballesteros21@gmail.com>
2024-12-02 23:47:27 +0800

    labels correction

--- resources/views/emails/receipts.blade.php
+++ resources/views/emails/receipts.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Payment Receipt</title>
+    <title>Payment Invoice</title>
     <style>
         body {
             font-family: Arial, sans-serif;
